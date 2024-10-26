41804ef45c2ba00bf89e6417a054e9505a952120
Emil Khodzhakhov <hodzhakhov@gmail.com>
2024-10-26 15:28:00 +0300

    fix

--- scripts/add.js
+++ scripts/add.js
@@ -1,38 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const BackButton = window.Telagram.WebApp.BackButton;
-    BackButton.show();
-    BackButton.onClick(() => {
-        window.location.href = 'index.html';
-    });
+let BackButton = window.Telagram.WebApp.BackButton;
+BackButton.show();
 
-    const fullAddress = document.getElementById('fullAddress').textContent;
-    const displayAddress = document.getElementById('displayAddress');
-    const copyButton = document.getElementById('copyButton');
-    const copyNotification = document.getElementById('copyNotification');
+const fullAddress = document.getElementById('fullAddress').textContent;
+const displayAddress = document.getElementById('displayAddress');
+const copyButton = document.getElementById('copyButton');
+const copyNotification = document.getElementById('copyNotification');
 
-    function shortenAddress(address, startLength = 4, endLength = 3) {
-        if (address.length > startLength + endLength) {
-            return address.slice(0, startLength) + '...' + address.slice(-endLength);
-        }
-        return address;
+function shortenAddress(address, startLength = 4, endLength = 3) {
+    if (address.length > startLength + endLength) {
+        return address.slice(0, startLength) + '...' + address.slice(-endLength);
     }
+    return address;
+}
 
-    displayAddress.textContent = shortenAddress(fullAddress);
+displayAddress.textContent = shortenAddress(fullAddress);
 
-    copyButton.addEventListener('click', () => {
-        window.focus();
-        if (navigator.clipboard) {
-            navigator.clipboard.writeText(fullAddress).then(() => {
-                copyNotification.style.display = 'block';
+copyButton.addEventListener('click', () => {
+    window.focus();
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(fullAddress).then(() => {
+            copyNotification.style.display = 'block';
 
-                setTimeout(() => {
-                    copyNotification.style.display = 'none';
-                }, 2000);
-            }).catch(err => {
-                console.error('Не удалось скопировать адрес: ', err);
-            });
-        } else {
-            console.error('Clipboard API не поддерживается этим браузером.');
-        }
-    });
+            setTimeout(() => {
+                copyNotification.style.display = 'none';
+            }, 2000);
+        }).catch(err => {
+            console.error('Не удалось скопировать адрес: ', err);
+        });
+    } else {
+        console.error('Clipboard API не поддерживается этим браузером.');
+    }
 });
