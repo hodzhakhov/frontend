0071340c7e88c2a4da9e9cd3bbc82153a84e2e76
Emil Khodzhakhov <hodzhakhov@gmail.com>
2024-12-21 01:38:59 +0300

    Add back button 2

--- scripts/crypto_table.js
+++ scripts/crypto_table.js
@@ -1,5 +1,35 @@
+function sessionStorageSet(key, value) {
+  try {
+    window.sessionStorage.setItem("__telegram__" + key, JSON.stringify(value));
+    return true;
+  } catch (e) {}
+  return false;
+}
+
+function sessionStorageGet(key) {
+  try {
+    return JSON.parse(window.sessionStorage.getItem("__telegram__" + key));
+  } catch (e) {}
+  return null;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  const tg = Telegram.WebApp;
+  var appTgVersion = 7.0;
+
+  var initParams = sessionStorageGet("initParams");
+  if (initParams) {
+    if (!initParams.tgWebAppVersion) {
+      initParams.tgWebAppVersion = appTgVersion;
+    }
+  } else {
+    initParams = {
+      tgWebAppVersion: appTgVersion,
+    };
+  }
+
+  sessionStorageSet("initParams", initParams);
+
+  const tg = window.Telegram.WebApp;
   tg.BackButton.show();
 
   const goBack = () => {
